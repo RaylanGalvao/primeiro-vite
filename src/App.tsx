38fa7b967de00742e880190c20e83c991eb3bdd2
RaylanGalvao <153932047+RaylanGalvao@users.noreply.github.com>
2024-06-01 13:49:06 -0300

    fixando useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
 import { useState } from "react"
+
+interface infoAlunoProps{
+  nome: string
+  idade: string
+}
+interface UseProps{
+  apelido: string
+  cargo: string
+}
 export default function App(){
   const [input, setInput] = useState("")
-  const [aluno, setAluno] = useState("sem nenhum nome")
   const [idade, setIdade] = useState("")
+  const [Contador, setContador] = useState(0)
+
+  const [infoAluno, setInfoAluno] = useState<infoAlunoProps>()
+  const [use, setUser] = useState({
+    apelido: "Visitante",
+    cargo: ""
+  })
 
   function mostraAluno(){
-    setAluno(input)
+    setInfoAluno({
+      nome: input,
+      idade: idade
+    })
+  }
+  function adicionar(){
+    setContador(valorAtual => valorAtual+1)
+  }
+  function diminuir(){
+    if(Contador===0){
+      return
+    }
+    setContador(valorAtual => valorAtual-1)
+  }
+  function HandleLogin(){
+    setUser({
+      apelido:"programador raiz",
+      cargo: "fullStack"
+    })
+  }
+  function handleLogout(){
+    setUser({
+      apelido: "visitante",
+      cargo:""
+    })
   }
 
   return(
@@ -27,9 +66,18 @@ export default function App(){
       <br/>
       <button onClick={mostraAluno}>Mostrar aluno</button>
       <hr />
-      <h3>bem vindo: {aluno}</h3>
-      <h3>idade: {idade} </h3>
-
+      <h3>bem vindo: {infoAluno?.nome}</h3>
+      <h3>idade: {infoAluno?.idade}  </h3>
+      <hr />
+      <br />
+      <h1>Contador de useState</h1>
+      <button onClick={adicionar}>+</button> { Contador } <button onClick={diminuir}>-</button> 
+      <br /><br /><hr />
+      <h1>Fixando useState</h1>
+      <button onClick={HandleLogin}>Entrar</button>
+      <button onClick={handleLogout}>Fazer logout</button>
+      <h1>Olá {use.apelido}</h1>
+      <strong>{use.cargo}</strong>
     </div>
   )
 }
